3e57f261542ce754e91ab7f0a13c22021b95b860
palustris610 <ktmkkl@gmail.com>
2017-12-16 23:29:21 +0100

    some notes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # gta-scripting-test
 
-blablabla
+this mod intends to be a racing gamemode and a race creator for gta fivem
+long term project, first ever mod
--- client.lua
+++ client.lua
@@ -3,7 +3,7 @@
 	--RP style? go into building to start a race, setup locations, money(entry fee, car, tuning, bets)
 	--how to test during creating?
 	--only 1 race per session? yes!
-	--creator or racing - separate mod, so everyone can make their own? which one to start with?
+	--creator or racing - separate mod, so everyone can make their own? which one to start with? i think the race
     --
 -- race creator? -creating races so it's not HARDCODED database sourced
 	--race options (optionLOCK, population, min/max laps, starting grid)
@@ -24,20 +24,24 @@
 --Marker types: https://wiki.gtanet.work/index.php?title=Marker
 --Blip types: https://wiki.gtanet.work/index.php?title=Blips
 
-
-local table = {
-	{x = 1,y = 1,z = 1},
-	{x = 2,y = 2,z = 2},
-	{x = 3,y = 3,z = 3}
+--race start or creator marker: replayicon id:24 OR id: 1 and 4 = cylinder and flag
+--need table for racestart and racecreator triggers
+local racingTriggers = {
+	{x = 1,y = 1,z = 1, type = 0},
+	{x = 2,y = 2,z = 2, type = 0},
+	{x = 3,y = 3,z = 3, type = 0},
+	{x = 4,y = 4,z = 4, type = 1},
+	{x = 5,y = 5,z = 5, type = 1},
+	{x = 6,y = 6,z = 6, type = 1}
 }
 
 -- Draw Markers -- 
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
-		for k in pairs(table) do
+		for k in pairs(racingTriggers) do
 			-- Draw Marker Here --
-			DrawMarker(1, table[k].x, table[k].y, table[k].z, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 0, 0, 255, 200, 0, 0, 0, 0)
+			DrawMarker(1, racingTriggers[k].x, racingTriggers[k].y, racingTriggers[k].z, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 0, 0, 255, 200, 0, 0, 0, 0)
 		end
 	end
 end)
@@ -46,15 +50,15 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 
-		for k in pairs(table) do
+		for k in pairs(racingTriggers) do --foreach marker in racingTriggers
 
-			local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
-			local dis = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, table[k].x, table[k].y, table[k].z)
+			local plyCoords = GetEntityCoords(GetPlayerPed(-1), false) --get player coords
+			local dis = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, racingTriggers[k].x, racingTriggers[k].y, racingTriggers[k].z) --distance
 
-			if dist <= 1.2 then
-				if IsControlJustPressed(1, 51) then
-                    pP = GetPlayerPed(-1)
-					SetEntityCoords(pP, x, y, z)
+			if dist <= 1.2 then --distance less than
+				if IsControlJustPressed(1, 51) then --pressed button
+                    pP = GetPlayerPed(-1) --get playerid
+					SetEntityCoords(pP, x, y, z) --teleport to coord
 				end
 			end
 		end
